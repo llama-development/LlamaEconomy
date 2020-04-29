5f6dc98a8ac5e97b202a78e956ff3632d37be489
Proxma <39440218+ProxmaDev@users.noreply.github.com>
2020-04-29 21:45:50 +0200

    1.0.1
    [+] Added UUID Support for the API.

--- src/main/java/net/lldv/LlamaEconomy/utils/API.java
+++ src/main/java/net/lldv/LlamaEconomy/utils/API.java
@@ -5,6 +5,7 @@ import net.lldv.LlamaEconomy.LlamaEconomy;
 import net.lldv.LlamaEconomy.provider.BaseProvider;
 
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class API {
@@ -17,6 +18,11 @@ public class API {
 
     public void saveAll(boolean async) {
         provider.saveAll(async);
+    }
+
+    public boolean hasAccount(UUID uuid) {
+        String name = LlamaEconomy.getInstance().getServer().getOfflinePlayer(uuid).getName();
+        return this.hasAccount(name);
     }
 
     public boolean hasAccount(Player player) {
@@ -35,12 +41,22 @@ public class API {
         provider.createAccount(username, money);
     }
 
+    public double getMoney(UUID uuid) {
+        String name = LlamaEconomy.getInstance().getServer().getOfflinePlayer(uuid).getName();
+        return this.getMoney(name);
+    }
+
     public double getMoney(Player player) {
         return this.getMoney(player.getName());
     }
 
     public double getMoney(String username) {
         return provider.getMoney(username);
+    }
+
+    public void setMoney(UUID uuid, double money) {
+        String name = LlamaEconomy.getInstance().getServer().getOfflinePlayer(uuid).getName();
+        this.setMoney(name, money);
     }
 
     public void setMoney(Player player, double money) {
@@ -51,12 +67,22 @@ public class API {
         CompletableFuture.runAsync(() -> provider.setMoney(username, money));
     }
 
+    public void addMoney(UUID uuid, double money) {
+        String name = LlamaEconomy.getInstance().getServer().getOfflinePlayer(uuid).getName();
+        this.addMoney(name, money);
+    }
+
     public void addMoney(Player player, double money) {
         this.addMoney(player.getName(), money);
     }
 
     public void addMoney(String username, double money) {
         CompletableFuture.runAsync(() -> provider.setMoney(username, provider.getMoney(username) + money));
+    }
+
+    public void reduceMoney(UUID uuid, double money) {
+        String name = LlamaEconomy.getInstance().getServer().getOfflinePlayer(uuid).getName();
+        this.reduceMoney(name, money);
     }
 
     public void reduceMoney(Player player, double money) {
